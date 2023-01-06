63d31502206204a30e69be6cad643ddc397da94e
carraes <carraeshb@gmail.com>
2023-01-06 16:02:56 -0300

    feat: works with html files

--- main.go
+++ main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Home page")
+	renderTemplate(w, "./templates/home.html")
 }
 
 func About(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "About page")
+	renderTemplate(w, "./templates/about.html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	temp, err := template.ParseFiles(tmpl)
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(w, nil)
 }
 
 func main() {
